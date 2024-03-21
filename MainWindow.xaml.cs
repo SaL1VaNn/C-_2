b1098c2bd5bdd9f7a80e68cc8e4413d54003e2ab
Denis Mashtak <127735395+SaL1VaNn@users.noreply.github.com>
2024-03-21 11:49:06 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        Matrix matrix;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculateResultButton.IsEnabled = false;
+        }
+
+        private void createMatrix_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int rows = int.Parse(textBoxRows.Text);
+                int columns = int.Parse(textBoxColumns.Text);
+
+                matrix = new Matrix(rows, columns);
+                int max = int.Parse(textBoxMax.Text);
+                int min = int.Parse(textBoxMin.Text);
+                matrix.FillElementsRandom(min, max);
+                Print(matrix);
+                calculateResultButton.IsEnabled = true;
+            }catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void Print(Matrix matrix)
+        {
+            DataTable dt = new DataTable();
+            int columns = matrix.columnCount;
+            int rows = matrix.rowCount;
+            for(int i =0; i < rows; i++)
+            {
+                dt.Columns.Add(i.ToString(), typeof(double));
+            }
+            for(int row = 0; row < rows; row++) {
+            DataRow dr = dt.NewRow();
+            for(int col = 0; col < columns; col++)
+                {
+                    dr[col] = matrix[row, col];
+                }
+            dt.Rows.Add(dr);
+            }
+            dataGrid.ItemsSource = dt.DefaultView;
+            dataGrid.HeadersVisibility = DataGridHeadersVisibility.None;
+            dataGrid.CanUserAddRows = false;
+        }
+
+        private void PrintResult(Matrix matrix)
+        {
+            result1.Content = "Суми елементів пизитивних рядків:";
+            int[] sumPosRows = matrix.GetPositiveRowsSum();
+            for(int i = 0; i < sumPosRows.Length; i++)
+            {
+                result1.Content += ' ' + sumPosRows[i].ToString();
+            }
+            int[] minSums = matrix.GetSumDiagonals();
+            int minSum = minSums[0];
+            for(int i = 0; i < minSums.Length; i++)
+            {
+                if (minSums[i] < minSum)
+                {
+                    minSum = minSums[i];
+                }
+            }
+            result2.Content = "Мінімальна сума, серед всіх сум елементів діагоналей: " + minSum.ToString();
+        }
+
+        private void calculateResultButton_Click(object sender, RoutedEventArgs e)
+        {
+            PrintResult(matrix);
+        }
+    }
+}
---
+++ Matrix.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Matrix
+    {
+        #region Поля 
+        int[,] matrix;
+        int[] positive_rows_sum;
+        int[] sum_diagonal;
+        #endregion
+
+        #region Властивості та індексатори 
+        public int rowCount {  get; set; }
+        public int columnCount { get; set; }
+
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i < rowCount && i >= 0 && j < columnCount && j >= 0)
+                    return matrix[i, j];
+                else throw new IndexOutOfRangeException("Індекс виходить за межі масиву!");
+            }
+        }
+        #endregion
+
+        #region Конструктори 
+        public Matrix(int rows, int columns) {
+        matrix = new int[rows, columns];
+        rowCount = matrix.GetLength(0);
+        columnCount = matrix.GetLength(1);
+        }
+        #endregion
+
+        #region Методи 
+        public void FillElementsRandom(int min, int max)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < columnCount; j++)
+                    matrix[i, j] = rand.Next(min, max + 1);
+        }
+        public int[] GetPositiveRowsSum()
+        {
+            positive_rows_sum = new int[rowCount];
+            for (int i = 0; i < rowCount; i++)
+            {
+                bool hasNegative = false;
+                int sum = 0;
+                for (int j = 0; j < columnCount; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        hasNegative = true;
+                        break;
+                    }
+                    sum += matrix[i, j];
+                }
+                if (!hasNegative)
+                    positive_rows_sum[i] = sum;
+            }
+            return positive_rows_sum.Where(x => x != 0).ToArray();
+            
+        }
+        public int[] GetSumDiagonals()
+        {
+            int maxDiagonals = Math.Min(rowCount, columnCount);
+            sum_diagonal = new int[maxDiagonals];
+
+            for (int k = 0; k < maxDiagonals; k++)
+            {
+                int sum = 0;
+                for (int i = 0, j = k; i < rowCount && j < columnCount; i++, j++)
+                {
+                    sum += matrix[i, j];
+                }
+                sum_diagonal[k] = sum;
+            }
+
+            return sum_diagonal;
+        }
+
+        #endregion
+    }
+}
